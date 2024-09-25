0308871e5249fab9308b9fec41a0768b839f45c2
FyndPrash <prashantpoojary@gofynd.com>
2024-09-25 11:51:43 +0530

    Initial commit

---
+++ sidebars.js
@@ -0,0 +1,25 @@
+const sidebars = {
+  tutorialSidebar: [
+    {
+      type: 'category',
+      label: 'Marketplace', 
+      items: [
+        'intro',
+        'tutorial-basics/create-a-blog-post',
+        'tutorial-basics/create-a-document',
+        'tutorial-basics/create-a-page',
+        'tutorial-basics/markdown-features',
+      ],
+    },
+    {
+      type: 'category',
+      label: 'Supply', 
+      items: [
+        'tutorial-extras/manage-docs-versions',
+        'tutorial-extras/translate-your-site',
+      ],
+    },
+  ],
+};
+
+export default sidebars;
